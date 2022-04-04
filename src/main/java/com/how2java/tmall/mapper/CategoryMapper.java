0f6707a894b2fcbfec470d38f74f57c37974ae8e
douzhenjun <doudouzhenjun@163.com>
2022-04-04 21:10:51 +0800

    commit

--- src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
+++ src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
@@ -6,9 +6,7 @@ import com.how2java.tmall.pojo.Category;
 import java.util.List;
  
 public interface CategoryMapper {
-    public List<Category> list(Page page);
-
-    public int total();
+    public List<Category> list();
     
     public void add(Category category);
     
--- src/main/java/com/how2java/tmall/service/CategoryService.java
+++ src/main/java/com/how2java/tmall/service/CategoryService.java
@@ -5,9 +5,8 @@ import com.how2java.tmall.util.Page;
 import java.util.List;
 
 public interface CategoryService{
-    int total();
     
-    List<Category> list(Page page);
+    List<Category> list();
     
     void add(Category category);
     
